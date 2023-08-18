ed1b8d934646e0ad5362976e9508918f6d1780e4
tildejustin <spacepepper3.14@gmail.com>
2023-08-17 20:29:27 -0400

    1.8-1.13

--- src/main/java/link/infra/motiono/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/link/infra/motiono/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,14 +1,14 @@
 package link.infra.motiono.mixin;
 
-import net.minecraft.entity.player.ClientPlayerEntity;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ClientPlayerEntity.class)
-public abstract class ClientPlayerEntityMixin {
-    @Inject(method = "method_1305", at = @At(value = "HEAD"), cancellable = true)
+@Mixin(AbstractClientPlayerEntity.class)
+public abstract class AbstractClientPlayerEntityMixin {
+    @Inject(method = "getSpeed", at = @At(value = "HEAD"), cancellable = true)
     public void onGetSpeed(CallbackInfoReturnable<Float> cir) {
         cir.setReturnValue(1.0f);
     }
